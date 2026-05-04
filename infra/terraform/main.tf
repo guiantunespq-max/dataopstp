c6a2f4212c8d1085513d4e3e24d15465fdb80e95
guiantunespq-max <guiantunespq@gmail.com>
2026-05-04 19:53:46 -0300

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "meu_bucket" {
-  bucket = "meu-bucket-dataops-guian-2026"
-
-  tags = {
-    Name        = "Bucket DataOps"
-    Environment = "Dev"
-    Projeto     = "Disciplina DataOps"
-  }
-}
+# resource "aws_s3_bucket" "meu_bucket" {
+#   bucket = "meu-bucket-dataops-guian-2026"
+#
+#   tags = {
+#     Name        = "Bucket DataOps"
+#     Environment = "Dev"
+#     Projeto     = "Disciplina DataOps"
+#   }
+# }
